1df15ca19c38b538a954f37817138ae3e6904be4
ThevenonT <thevenon.thomas@hotmail.com>
2022-06-20 13:08:04 +0200

    order number display

---
+++ front/js/confirmation.js
@@ -0,0 +1,15 @@
+/**
+ * récupère l'id de commande dans le localStorage.getItem('orderId');
+ * @return id de commande 
+ */
+let url = new URL(window.location.href);
+let search_params = new URLSearchParams(url.search);
+if (search_params.has('orderId')) {
+    _id = search_params.get('orderId');
+}
+console.log('_id: ', _id);
+console.log(localStorage);
+
+let orderId = document.getElementById('orderId');
+orderId.innerHTML = _id;
+
