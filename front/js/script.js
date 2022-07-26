6b83033f36e8591049a6950268ad1836be9a08b0
ThevenonT <thevenon.thomas@hotmail.com>
2022-07-26 10:22:11 +0200

    comment modify

--- front/js/script.js
+++ front/js/script.js
@@ -1,35 +1,34 @@
 /**
- * query the api products
- * @return all the products present
+ * @return tout les produits présent 
  */
 fetch('http://localhost:3000/api/products')
 
-  // receive all products in json format
+  // récupère tous les produit et les passent au format json 
   .then(function (res) {
     if (res.ok) {
       return res.json();
     }
   })
 
-  // put all the products in a variable 
+  // récupère tous les produits dans une variable 
   .then(function (value) {
     const section = document.getElementById('items');
 
-    // return the products individually
+    // retourne les produits individuellement 
     for (let valu of value) {
 
-      // create an <a> tag and add the attributes of each product
+      // créer une balise <a> et ajoute les attributs de chaque produit
       const a = document.createElement('a');
       a.setAttribute('href', './product.html?id=' + valu._id);
 
-      // create an <article> tag and add it to the tag <section>
+      // créez une balise <article> et l'ajoute à la balise <section>
       const article = document.createElement('article');
       section.appendChild(a);
 
-      // add the <article> tag inside the <a> tag
+      // ajoute la balise <article> à l'intérieur de la balise <a>
       a.appendChild(article);
 
-      // add image, title and description in the <article> tag
+      // ajoute une image, un titre et une description dans la balise <article>
       article.innerHTML = "<img src=" + valu.imageUrl + " alt='Lorem ipsum dolor sit amet, Kanap name1'>" +
         "<h3 class='productName'>" + valu.name + "</h3>" +
         "<p class='productDescription'>" + valu.description + "</p>";
@@ -43,7 +42,7 @@ fetch('http://localhost:3000/api/products')
   })
   .catch(function (err) {
 
-    // An error has occurred
+    // Si une erreur si produit 
     console.log("il y a une erreur " + err);
   });
 
